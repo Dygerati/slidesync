7bf4d7757389f52c4f5c1b9e683eccc22e00fbc9
Joel T <dygerati@gmail.com>
2013-09-10 16:22:30 -0700

    Restructured server code to make it more modular

--- slideSync/slide-sync.js
+++ slideSync/slide-sync.js
@@ -1,59 +1,104 @@
 
-module.exports = function(http) {
-	
+var rS;
+
+function slideSync(http) {
+
+	if ( arguments.callee._singletonInstance )
+		return arguments.callee._singletonInstance;
+	arguments.callee._singletonInstance = this;
+
+	rS = this;
+
 	var io = require('socket.io').listen(http);
 
-	io.of('/sync').on('connection', function(socket) {
-		socket.emit('serverMessage', 'testing123');
+	this.currentAdmin = '';
+
+	io.of('/sync').on('connection', this.onConnection);
+}
+
+slideSync.prototype = {
+
+	onConnection: function(socket) {
+
+		if(rS.currentAdmin) {
+
+			socket.emit('controlAssigned', rS.currentAdmin);
+
+		}
 
 		socket.on('takeControl', function(name){
-			socket.set('admin', true, function(err) {
-				handleError(err);
-
-				socket.broadcast.emit('controlAssigned', name);
-			});
+			rS.onTakeControl(socket, name);
 		});
 
-		socket.on('releaseControl', function() {
-
-			socket.get('admin', function(err, admin) {
-
-				handleError(err);
-
-				if(admin) {
-
-					socket.set('admin', false);
-					socket.broadcast.emit('controlReleased');
-				}
-
-			});
-
+		socket.on('releaseControl', function(){
+			rS.onReleaseControl(socket);
 		});
 
 		socket.on('navigateTo', function(index){
+			rS.onNavigateTo(socket, index);
+		});
 
-			socket.get('admin', function(err, admin) {
+		socket.on('disconnect', function(){
+			rS.onReleaseControl(socket);
+		});
 
-				handleError(err);
+	},
 
-				if(admin) {
+	onTakeControl: function(socket, name){
+		socket.set('admin', true, function(err) {
+			handleError(err);
 
-					socket.broadcast.emit('navigateTo', index);
+			rS.currentAdmin = name;
 
-				}
+			socket.broadcast.emit('controlAssigned', name);
+		});
+	},
 
-			});
+	onReleaseControl: function(socket) {
+
+		socket.get('admin', function(err, admin) {
+
+			handleError(err);
+
+			if(admin) {
+
+				socket.set('admin', false);
+				socket.broadcast.emit('controlReleased');
+
+				rS.currentAdmin = '';
+			}
 
 		});
 
-	});
+	},
 
-	function handleError(err) {
+	onNavigateTo: function(socket, index){
 
-		if(err) {
-			console.log('Error: ', err.msg);	
-		}
-		
+		socket.get('admin', function(err, admin) {
+
+			handleError(err);
+
+			if(admin) {
+
+				socket.broadcast.emit('navigateTo', index);
+
+			}
+
+		});
+
 	}
+};
 
+
+function handleError(err) {
+
+	if(err) {
+		console.log('Error: ', err.msg);	
+	}
+	
+}
+
+
+module.exports = function(http) {
+	return new slideSync(http);
 }
